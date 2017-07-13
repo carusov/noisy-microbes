4113d2e6e6330f070c885535dd6556021bb237ac
Vincent Caruso <carusov@ohsu.edu>
2017-07-12 17:30:25 -0700

    Remove initial truncate reads, add create OTU table

--- scripts/uparse_pipeline.sh
+++ scripts/uparse_pipeline.sh
@@ -6,17 +6,9 @@
 ### (see www.drive5.com/usearch for detailed documentation)
 
 DATA_PATH=~/projects/thesis/data
-
-# First truncate low-quality base calls at the beginning and end of the reads
-for fq in ls $DATA_PATH/raw/*.fastq
-do
-    bn=$(basename $fq .fastq)
-    usearch -fastq_filter $DATA_PATH/$bn -fastq_trunclen 220 -fastqout $DATA_PATH/clean/$bn".tmp"
-    usearch -fastq_filter $DATA_PATH/clean/$bn".tmp" -fastq_stripleft 20 -fastqout $DATA_PATH/clean/$bn"_trunc.fastq"
-done
 	  
-# Then merge forward and reverse reads from each sample, and pool the merged reads
-usearch -fastq_mergepairs $DATA_PATH/clean/*R1_trunc.fastq \
+# First merge forward and reverse reads from each sample, and pool the merged reads
+usearch -fastq_mergepairs $DATA_PATH/raw/*R1.fastq \
 	-fastqout $DATA_PATH/clean/pooled_merged.fastq \
 	-fastq_maxdiffs 100 -fastq_pctid 50 -fastq_minmergelen 245 -fastq_maxmergelen 255 \
 	-relabel @ -report $DATA_PATH/reports/merge_report.txt
@@ -24,7 +16,7 @@ usearch -fastq_mergepairs $DATA_PATH/clean/*R1_trunc.fastq \
 # Create a report of the expected errors of the merged reads
 usearch -fastq_eestats2 $DATA_PATH/clean/pooled_merged.fastq \
 	-output $DATA_PATH/reports/merged_eestats.txt \
-	-length_cutoffs 200,*,10 -ee_cutoffs 1.0,2.0,2.5,3.0,3.5,4.0
+	-length_cutoffs 200,*,10
 
 # Quality filter the reads using a maximum of 2.0 expected errors, after trimming nucleotides from the
 # beginning and end of the reads
@@ -32,9 +24,15 @@ usearch -fastq_filter $DATA_PATH/clean/pooled_merged.fastq -fastqout $DATA_PATH/
 	-fastq_maxee 2.0
 
 # Dereplicate the reads
-usearch -fastx_uniques $DATA_PATH/clean/all_filtered.fastq -fastqout $DATA_PATH/clean/pooled_uniques.fastq \
+usearch -fastx_uniques $DATA_PATH/clean/pooled_filtered.fastq -fastqout $DATA_PATH/clean/pooled_uniques.fastq \
 	-sizeout -relabel Uniq
 
 # Now cluster the reads into OTUs using the UPARSE algorithm
 usearch -cluster_otus $DATA_PATH/clean/pooled_uniques.fastq -otus $DATA_PATH/uparse_otus/otus.fa \
 	-relabel OTU
+
+# Use the generated OTUs to create an OTU table
+usearch -otutab $DATA_PATH/clean/pooled_merged.fastq -otus $DATA_PATH/uparse_otus/otus.fa \
+	-otutabout $DATA_PATH/uparse_otus/otutab.txt -biomout $DATA_PATH/uparse_otus/otutab.biom \
+	-mapout $DATA_PATH/uparse_otus/map.txt -dbmatched $DATA_PATH/uparse_otus/otus_with_sizes.fa \
+	-sizeout
