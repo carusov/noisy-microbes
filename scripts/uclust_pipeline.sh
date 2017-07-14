c455e6cc182b95cad66743c2ccbf0c9f3c8ea781
Qiime User <qiime@qiime-190-virtual-box.(none)>
2017-07-13 17:08:47 -0700

    Remove usearch cluster command, add QIIME scripts

--- scripts/uclust_pipeline.sh
+++ scripts/uclust_pipeline.sh
@@ -1,18 +1,61 @@
 #!/bin/bash
 
-usearch -fastq_mergepairs raw/*R1.fastq -fastqout clean/all_merged.fastq \
-	-fastq_maxdiffs 100 -fastq_pctid 50 -fastq_minmergelen 245 -fastq_maxmergelen 255 \
-	-relabel @ -report reports/merge_report.txt
+### Author: Vincent Caruso
+### Date: 7/13/2017
+### This script implements the UCLUST pipeline through the QIIME SOP
+### (see www.qiime.org for detailed documentation)
 
-usearch -fastq_eestats2 clean/all_merged.fastq -output reports/merged_eestats.txt \
-	-length_cutoffs 200,*,10 -ee_cutoffs 1.0,2.0,2.5,3.0,3.5,4.0
+DATA=~/projects/thesis/data
 
-usearch -fastq_filter clean/all_merged.fastq -fastqout clean/all_filtered.fastq \
-	-fastq_maxee 3.0
+# First merge forward and reverse reads from each sample, and pool the merged reads
+usearch -fastq_mergepairs $DATA/raw/*R1.fastq \
+        -fastqout $DATA/clean/pooled_merged.fastq \
+	-fastq_maxdiffs 100 \
+        -fastq_pctid 50 \
+        -fastq_minmergelen 245 \
+        -fastq_maxmergelen 255 \
+	-relabel @ \
+        -report $DATA/reports/merge_report.txt
 
-usearch -fastx_uniques clean/all_filtered.fastq -fastqout clean/all_uniques.fastq \
-	-sizeout -relabel Uniq
+# Create report of the expected errors of the merged reads
+usearch -fastq_eestats2 $DATA/clean/pooled_merged.fastq \
+        -output $DATA/reports/merged_eestats.txt \
+	-length_cutoffs 200,*,10 \
 
-usearch -cluster_fast clean/s160_MC_Neat_filtered.fastq -id 0.97 \
-	-centroids clustered/centroids.fasta -uc clustered/clusters.uc \
-	-sort size -maxaccepts 1 -maxrejects 80
+# Quality filter the reads using a maximum of 2.0 expected errors
+usearch -fastq_filter $DATA/clean/pooled_merged.fastq \
+        -fastqout $DATA/clean/pooled_filtered.fastq \
+	-fastq_maxee 2.0
+
+# Reformat sequence files to QIIME format
+usearch -fastq_filter $DATA/clean/pooled_filtered.fastq \
+        -fastaout $DATA/clean/pooled_filtered.fasta \
+        -fastq_ascii 64
+
+### The rest of the pipeline is executed with QIIME scripts
+
+# Identify chimeric sequences 
+identify_chimeric_seqs.py -m usearch61 \
+        -i $DATA/clean/pooled_filtered.fasta \
+        -r $DATA/references/gold.fa \
+        -o $DATA/uchime/
+
+# Filter out identified chimeric sequences
+filter_fasta.py -f $DATA/clean/pooled_filtered.fasta \
+        -o $DATA/clean/pooled_nochim.fa \
+        -s $DATA/uchime/chimeras.txt \
+        -n 
+
+# Pick de novo OTUs
+# I need to investigate the parameters that this QIIME script passes to UCLUST. Are sequences sorted by size?
+pick_de_novo_otus.py -i $DATA/clean/pooled_nochim.fa \
+        -o $DATA/uclust/ \
+        -f
+
+# Dereplicate 
+# usearch -fastx_uniques clean/all_filtered.fastq -fastqout clean/all_uniques.fastq \
+#	-sizeout -relabel Uniq
+
+# usearch -cluster_fast clean/s160_MC_Neat_filtered.fastq -id 0.97 \
+#	-centroids clustered/centroids.fasta -uc clustered/clusters.uc \
+#	-sort size -maxaccepts 1 -maxrejects 80
