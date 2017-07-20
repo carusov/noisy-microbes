ef7eb8354e4f69328f7be990e63f5f862b48af31
Vincent Caruso <carusov@ohsu.edu>
2017-07-20 11:15:23 -0700

    Remove commented lines from fastq_info script

--- scripts/fastq_info.sh
+++ scripts/fastq_info.sh
@@ -2,11 +2,9 @@
 
 echo "This script generates summary stats on all .fastq files in the current working directory."
 
-#alias usearch='usearch10.0.240_i86linux32'
 mkdir -p ../fastq_info
 
 for fq in *.fastq
 do
-#  usearch -fastx_info $fq -output ../fastq_info/$fq
-  usearch10.0.240_i86linux32 -fastx_info $fq -output ../fastq_info/$fq
+  usearch -fastx_info $fq -output ../fastq_info/$fq
 done
