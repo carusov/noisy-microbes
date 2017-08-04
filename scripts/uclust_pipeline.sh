bf20dc14a2b997327514f58db127eec7b3a4bb8f
Vincent Caruso <carusov@ohsu.edu>
2017-08-04 10:38:53 -0700

    Add sed to format fasta for QIIME; add convert .biom to .txt

--- scripts/uclust_pipeline.sh
+++ scripts/uclust_pipeline.sh
@@ -11,11 +11,15 @@ RESULTS=~/projects/thesis/results
 # Activate the qiime environment in miniconda
 source activate qiime1
 
+# Convert .fastq to .fasta for use with QIIME
+printf "\nGenerating .fasta file from .fastq file\n"
+usearch -fastq_filter $DATA/clean/pooled_filtered.fastq \
+        -fastaout $DATA/clean/pooled_filtered.fasta
+
 # Reformat sequence files to QIIME format
 printf "\nReformatting read sequences to QIIME format...\n"
-usearch -fastq_filter $DATA/clean/pooled_filtered.fastq \
-        -fastaout $DATA/clean/pooled_filtered.fasta \
-        -fastq_ascii 64
+sed '/^>/ s/\./_/' $DATA/clean/pooled_filtered.fasta \
+    > $DATA/clean/pooled_filtered_qiime.fasta
 
 ### The rest of the pipeline is executed with QIIME scripts
 
@@ -26,13 +30,13 @@ fi
 # Identify chimeric sequences 
 printf "\nIdentifying chimeric sequences...\n"
 identify_chimeric_seqs.py -m usearch61 \
-        -i $DATA/clean/pooled_filtered.fasta \
+        -i $DATA/clean/pooled_filtered_qiime.fasta \
         -r $DATA/reference/gold.fa \
         -o $RESULTS/uclust/
 
 # Filter out identified chimeric sequences
 printf "\nRemoving chimeric sequences from sample reads...\n"
-filter_fasta.py -f $DATA/clean/pooled_filtered.fasta \
+filter_fasta.py -f $DATA/clean/pooled_filtered_qiime.fasta \
         -o $RESULTS/uclust/pooled_nochim.fa \
         -s $RESULTS/uclust/chimeras.txt \
         -n 
@@ -46,3 +50,8 @@ printf "phylogenetic tree...\n"
 pick_de_novo_otus.py -i $RESULTS/uclust/pooled_nochim.fa \
         -o $RESULTS/uclust/ \
         -f
+
+# Convert the .biom file to a tabbed .txt file
+biom convert -i $RESULTS/uclust/otu_table.biom \
+     -o $RESULTS/uclust/otu_table.txt \
+     --to-tsv
