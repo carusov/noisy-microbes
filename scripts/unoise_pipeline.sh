7f458526f8a09eab3b393065319fcc9e46201abf
Vincent Caruso <carusov@ohsu.edu>
2017-08-12 12:01:35 -0700

    Add command-line options

--- scripts/unoise_pipeline.sh
+++ scripts/unoise_pipeline.sh
@@ -7,65 +7,97 @@
 ### The input to the script is a .fastq file of merged, pooled, and quality-
 ### filtered sample reads
 
-DATA=~/projects/thesis/data
-RESULTS=~/projects/thesis/results
 
-if [ ! -d "$RESULTS/unoise" ]; then
-    mkdir $RESULTS/unoise
+# Set the default input file, output directory, and raw merged read file
+INFILE=~/projects/thesis/data/filtered/pooled_filtered.fastq
+OUTDIR=~/projects/thesis/results/unoise
+RAW_MERGED_FILE=~/projects/thesis/data/merged/pooled_merged.fastq
+
+# Parse command-line options
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+	-i|--input)
+	    INFILE="$2"
+	    shift;;
+	-o|--output)
+	    OUTDIR="$2"
+	    shift;;
+	-r|--raw-merged)
+	    RAW_MERGED_FILE="$2"
+	    shift;;
+	-h|--help)
+	    printf "\nUSAGE: unoise_pipeline.sh -i input_file -o output_directory -r raw_merged_reads_file\n\n"
+	    exit;;
+	*)
+
+	;;
+    esac
+    shift
+done
+
+printf "\nINPUT FILE = ""${INFILE}""\n"
+printf "OUTPUT DIRECTORY = ""${OUTDIR}""\n\n"
+
+# Create the output directory, if necessary
+if [ ! -d "$OUTDIR" ]; then
+    mkdir $OUTDIR
 fi
 
 # Dereplicate the reads
-usearch -fastx_uniques $DATA/clean/pooled_filtered.fastq \
-	-fastqout $DATA/clean/pooled_uniques.fastq \
+usearch -fastx_uniques $INFILE \
+	-fastqout $OUTDIR/pooled_uniques.fastq \
 	-sizeout \
 	-relabel Uniq
 
 # Now denoise the reads into ZOTUs using the UNOISE2 algorithm
-usearch -unoise3 $DATA/clean/pooled_uniques.fastq \
-	-zotus $RESULTS/unoise/zotus.fa \
-	-ampout $RESULTS/unoise/amplicons.fa \
-	-tabbedout $RESULTS/unoise/unoise3.txt
+usearch -unoise3 $OUTDIR/pooled_uniques.fastq \
+	-zotus $OUTDIR/zotus.fa \
+	-ampout $OUTDIR/amplicons.fa \
+	-tabbedout $OUTDIR/unoise3.txt
 
 # Filter out the ZOTUS from the predicted amplicons, leaving just the predicted chimeras
-usearch -search_exact $RESULTS/unoise/amplicons.fa \
-	-db $RESULTS/unoise/zotus.fa \
+usearch -search_exact $OUTDIR/amplicons.fa \
+	-db $OUTDIR/zotus.fa \
 	-strand plus \
-	-notmatched $RESULTS/unoise/chimeras.fa
+	-notmatched $OUTDIR/chimeras.fa
 
 # Use the generated OTUs to create an OTU table
-usearch -otutab $DATA/clean/pooled_merged.fastq \
-	-otus $RESULTS/unoise/zotus.fa \
-	-otutabout $RESULTS/unoise/zotu_table.txt \
-	-biomout $RESULTS/unoise/zotu_table.biom \
-	-mapout $RESULTS/unoise/map.txt \
-	-dbmatched $RESULTS/unoise/zotus_with_sizes.fa \
-	-notmatchedfq $RESULTS/unoise/unmapped_reads.fastq \
+usearch -otutab $RAW_MERGED_FILE \
+	-otus $OUTDIR/zotus.fa \
+	-otutabout $OUTDIR/zotu_table.txt \
+	-biomout $OUTDIR/zotu_table.biom \
+	-mapout $OUTDIR/map.txt \
+	-dbmatched $OUTDIR/zotus_with_sizes.fa \
+	-notmatchedfq $OUTDIR/unmapped_reads.fastq \
 	-sizeout
 
 # Find out how many reads didn't map to OTUs
-usearch -fastx_info $RESULTS/unoise/unmapped_reads.fastq \
-	-output $RESULTS/unoise/unmapped_reads_info.txt
+usearch -fastx_info $OUTDIR/unmapped_reads.fastq \
+	-output $OUTDIR/unmapped_reads_info.txt
 
 # How many "hiqh quality" reads don't map to OTUS?
-usearch -fastq_filter $RESULTS/unoise/unmapped_reads.fastq \
+usearch -fastq_filter $OUTDIR/unmapped_reads.fastq \
 	-fastq_maxee 2.0 \
-	-fastaout $RESULTS/unoise/unmapped_hiqual.fa \
-	-fastaout_discarded $RESULTS/unoise/unmapped_loqual.fa
+	-fastaout $OUTDIR/unmapped_hiqual.fa \
+	-fastaout_discarded $OUTDIR/unmapped_loqual.fa
 
 # Combine predicted OTUs and chimeras into a single database
-cat $RESULTS/unoise/zotus.fa $RESULTS/unoise/chimeras.fa \
-    > $RESULTS/unoise/otus_chimeras.fa
+cat $OUTDIR/zotus.fa $OUTDIR/chimeras.fa \
+    > $OUTDIR/otus_chimeras.fa
 
 # Now, see how many high-quality, unmapped sequences are within 5% of an OTU or chimeric sequence
-usearch -usearch_global $RESULTS/unoise/unmapped_hiqual.fa \
-	-db $RESULTS/unoise/otus_chimeras.fa \
+usearch -usearch_global $OUTDIR/unmapped_hiqual.fa \
+	-db $OUTDIR/otus_chimeras.fa \
 	-strand plus \
 	-id 0.95 \
-	-matched $RESULTS/unoise/unmatched_noisy.fa \
-	-notmatched $RESULTS/unoise/unmatched_hiqual_other.fa
+	-matched $OUTDIR/unmatched_noisy.fa \
+	-notmatched $OUTDIR/unmatched_hiqual_other.fa
 
 # Final coverage check: see if any leftover high-quality reads map to a large database
-#usearch -usearch_global $RESULTS/unoise/unmatched_hiqual_other.fa \
+#usearch -usearch_global $OUTDIR/unmatched_hiqual_other.fa \
 #	-db silva.udb \
 #	-strand both \
 #	-idd 0.99 \
