8f1e2c670bb1ffd2619966e1559af0817765814e
Vincent Caruso <carusov@ohsu.edu>
2017-09-11 19:33:21 -0700

    Get input file from command line

--- scripts/rmd2r.R
+++ scripts/rmd2r.R
@@ -1,5 +1,18 @@
+#!/usr/bin/Rscript
+
 # Convert a .Rmd file to a .R script
 library(knitr)
-setwd("~/projects/thesis/noisy-microbes/scripts")
-purl("dada2_pipeline.Rmd")
+library(optparse)
+
+option_list = list(
+  make_option(c("-i", "--in_file"), type = "character", default = NULL, 
+              help = "dataset file name", metavar = "character")
+  # make_option(c("-d", "--out_dir"), type = "character", 
+  #             default = "~projects/thesis/noisy-microbes/scripts")
+)
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+#setwd(opt$out_dir)
+purl(opt$in_file)
 q()
