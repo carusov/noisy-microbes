d2e23f476be361035c150cd2be8c69fc3f2c6a02
Vincent Caruso <carusov@ohsu.edu>
2017-09-11 20:05:24 -0700

    Get i/o/ref directories and seq lengths (for DADA2) from command line

--- scripts/run_all_pipelines.sh
+++ scripts/run_all_pipelines.sh
@@ -9,28 +9,86 @@
 ### on someone else's machine at the moment.
 ### Usage: run_all_pipelines.sh
 
-# Run the UCLUST pipeline with defaults
+# Set the default input, output, and reference directories
+INDIR=~/projects/thesis/data/dilution_w_blank
+OUTDIR=~/projects/thesis/results/dilution_w_blank
+REFDIR=~/projects/thesis/references
+
+# Set default 
+MIN_LEN=230
+MAX_LEN=235
+
+# Parse command-line options
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+	-i|--input)
+	    INDIR="$2"
+	    shift;;
+	-o|--output)
+	    OUTDIR="$2"
+	    shift;;
+	-r|--ref)
+	    REFDIR="$2"
+	    shift;;
+	-s|--min_len)
+	    MIN_LEN="$2"
+	    shift;;
+	-l|--max_len)
+	    MAX_LEN="$2"
+	    shift;;
+	-h|--help)
+	    printf "\nUSAGE: run_all_pipelines.sh -i input_directory -o output_directory -r reference_directory\n\n"
+	    exit;;
+	*)
+
+	;;
+    esac
+    shift
+done
+
+printf "\nINPUT DIRECTORY = ""${INDIR}""\n"
+printf "OUTPUT DIRECTORY = ""${OUTDIR}""\n"
+printf "REFERENCE DIRECTORY = ""${REFDIR}""\n\n"
+
+# Create the output directory, if necessary
+if [ ! -d "$OUTDIR" ]; then
+    mkdir $OUTDIR
+fi
+
+# Run the UCLUST pipeline
 printf "\nRunning the UCLUST pipeline...\n\n"
-uclust_pipeline.sh
+uclust_pipeline.sh -i $INDIR/filtered/pooled_filtered_qiime.fasta \
+		   -o $OUTDIR/uclust \
+		   -r $REFDIR/gold.fa
 
-# Run the UPARSE pipeline with defaults
+# Run the UPARSE pipeline
 printf "\nRunning the UPARSE pipeline...\n\n"
-uparse_pipeline.sh
+uparse_pipeline.sh -i $INDIR/filtered/pooled_filtered.fastq \
+		   -o $OUTDIR/uparse \
+		   -r $INDIR/merged/pooled_merged.fastq
 
-# Run the UNOISE pipeline with defaults
+# Run the UNOISE pipeline
 printf "\nRunning the UNOISE pipeline...\n\n"
-unoise_pipeline.sh
+unoise_pipeline.sh -i $INDIR/filtered/pooled_filtered.fastq \
+		   -o $OUTDIR/unoise \
+		   -r $INDIR/merged/pooled_merged.fastq
 
-# Run the MED pipeline with defaults
+# Run the MED pipeline
 printf "\nRunning the MED pipeline...\n\n"
-med_pipeline.sh
+med_pipeline.sh -i $INDIR/filtered/pooled_filtered_qiime.fasta \
+		-o $OUTDIR/med
 
-# Run the Deblur pipeline with defaults
+# Run the Deblur pipeline
 printf "\nRunning the Deblur pipeline...\n\n"
-deblur_pipeline.sh
+deblur_pipeline.sh -i $INDIR/filtered/pooled_filtered_qiime.fasta \
+		   -o $OUTDIR/deblur
 
 # Run the DADA2 pipeline with defaults
 printf "\nRunning the DADA2 pipeline...\n\n"
 SCRIPTS=~/projects/thesis/noisy-microbes/scripts
-Rscript $SCRIPTS/rmd2r.R
-Rscript $SCRIPTS/dada2_pipeline.R
+#$SCRIPTS/rmd2r.R -i $SCRIPTS/dada2_pipeline.Rmd -d $SCRIPTS
+Rscript $SCRIPTS/dada2_pipeline.R -i $INDIR -o $OUTDIR \
+	-s $MIN_LEN -l $MAX_LEN
