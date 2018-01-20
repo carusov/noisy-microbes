abd183cf830d763f4e7db4d8883875ef994a9d97
Vincent Caruso <carusov@ohsu.edu>
2018-01-19 17:18:56 -0800

    Include max targets in command line calls

--- scripts/blast_all.sh
+++ scripts/blast_all.sh
@@ -3,9 +3,9 @@
 ### This is a script to blast sequences from all six 16S processing methods at
 ### once.
 
-blast_seqs.sh -i uclust_seqs.fasta -o uclust_blast.txt
-blast_seqs.sh -i uparse_seqs.fasta -o uparse_blast.txt
-blast_seqs.sh -i unoise_seqs.fasta -o unoise_blast.txt
-blast_seqs.sh -i med_seqs.fasta -o med_blast.txt
-blast_seqs.sh -i deblur_seqs.fasta -o deblur_blast.txt
-blast_seqs.sh -i dada2_seqs.fasta -o dada2_blast.txt
+blast_seqs.sh -i uclust_seqs.fasta -o uclust_blast.txt -t 10
+blast_seqs.sh -i uparse_seqs.fasta -o uparse_blast.txt -t 10
+blast_seqs.sh -i unoise_seqs.fasta -o unoise_blast.txt -t 10
+blast_seqs.sh -i med_seqs.fasta -o med_blast.txt -t 10
+blast_seqs.sh -i deblur_seqs.fasta -o deblur_blast.txt -t 10
+blast_seqs.sh -i dada2_seqs.fasta -o dada2_blast.txt -t 10
