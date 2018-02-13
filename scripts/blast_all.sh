eaa9da5c7077a630c93481b46f61255b6758c25c
Vincent Caruso <carusov@ohsu.edu>
2018-02-13 15:01:46 -0800

    Add progress messages

--- scripts/blast_all.sh
+++ scripts/blast_all.sh
@@ -4,8 +4,14 @@
 ### once.
 
 blast_seqs.sh -i uclust_seqs.fasta -o uclust_blast.txt -t 10
+echo "Done with UCLUST BLAST search"
 blast_seqs.sh -i uparse_seqs.fasta -o uparse_blast.txt -t 10
+echo "Done with UPARSE BLAST search"
 blast_seqs.sh -i unoise_seqs.fasta -o unoise_blast.txt -t 10
+echo "Done with UNOISE BLAST search"
 blast_seqs.sh -i med_seqs.fasta -o med_blast.txt -t 10
+echo "Done with MED BLAST search"
 blast_seqs.sh -i deblur_seqs.fasta -o deblur_blast.txt -t 10
+echo "Done with Deblur BLAST search"
 blast_seqs.sh -i dada2_seqs.fasta -o dada2_blast.txt -t 10
+echo "Done with DADA2 BLAST search"
