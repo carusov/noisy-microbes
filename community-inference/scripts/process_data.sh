a846d4f554e2df2dd36bed1d4251f41331c30569
Vincent Caruso <carusov@ohsu.edu>
2019-09-30 22:56:15 -0400

    Add command-line parameter parsing

--- community-inference/scripts/process_data.sh
+++ community-inference/scripts/process_data.sh
@@ -2,16 +2,48 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+WDIR=""
+
+# Parse command-line options
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+	-w|--working_dir)
+	    WDIR="$2"
+	    shift;;
+	-h|--help)
+	    printf "\nUSAGE: process_data.sh [-w --working working_directory] [options]\n"
+	    printf "\n\n"
+	    exit;;
+	*)
+
+	;;
+    esac
+    shift
+done
+
+if [[ -z "$WDIR" ]]
+then
+    echo "ERROR: You must specify the top-level working directory."
+    exit 1
+fi
+
+
+WDIR=$(readlink -f "$WDIR")
+
+
 printf "\n**********************************************************************"
 printf "\n**********************************************************************\n\n"
 printf "\nProcessing high biomass samples...\n\n"
 printf "\n**********************************************************************"
 printf "\n**********************************************************************\n\n"
-process_high_biomass.sh
+process_high_biomass.sh -w "$WDIR"
 
 printf "\n**********************************************************************"
 printf "\n**********************************************************************\n\n"
 printf "\nProcessing dilution series samples...\n\n"
 printf "\n**********************************************************************"
 printf "\n**********************************************************************\n\n"
-process_dilution.sh
+process_dilution.sh -w "$WDIR"
--- community-inference/scripts/process_high_biomass.sh
+++ community-inference/scripts/process_high_biomass.sh
@@ -6,11 +6,49 @@ IFS=$'\n\t'
 ### dataset was processed, including the final parameters. It can
 ### also be run to re-process all datasets if desired.
 
-DATA=~/repo_test/data
-RESULTS=~/repo_test/results
+WDIR=""
 MAXDIFFS=10
 MAXEE=2.0
 MAXN=0
+
+
+# Parse command-line options
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+	-w|--working_dir)
+	    WDIR="$2"
+	    shift;;
+	-d|--maxdiffs)
+	    MAXDIFFS="$2"
+	    shift;;
+	-e|--maxee)
+	    MAXEE="$2"
+	    shift;;
+	-n|--maxns)
+	    MAXNS="$2"
+	    shift;;
+	-h|--help)
+	    printf "\nUSAGE: process_high_biomass.sh [-w --working working_directory] [options]\n"
+	    printf "\nOptions: \t[default]"
+	    printf "\n-e --maxee \t["$MAXEE"] \t\t\t\t\tmax_expected_errors"
+	    printf "\n-d --maxdiffs \t["$MAXDIFFS"] \t\t\t\t\tmax_differences"
+	    printf "\n-n --maxns \t["$MAXN"] \t\t\t\t\tmax_Ns"
+	    printf "\n\n"
+	    exit;;
+	*)
+
+	;;
+    esac
+    shift
+done
+
+
+WDIR=$(readlink -f "$WDIR")
+DATA="$WDIR"/data
+RESULTS="$WDIR"/results
 
 
 printf "\n**********************************************************************"
